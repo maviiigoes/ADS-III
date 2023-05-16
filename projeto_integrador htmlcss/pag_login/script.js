6979da487a4136f6055c52eda113c3b4e5c61369
Maria Vitória S.G <110963488+maviiigoes@users.noreply.github.com>
2023-05-16 15:25:57 -0300

    Add files via upload

---
+++ projeto_integrador htmlcss/pag_login/script.js
@@ -0,0 +1,7 @@
+function login() {
+    var email = document.getElementById("email").value;
+    var senha  = document.getElementById("senha").value;
+    var botao_entrar = document.getElementById(entrar)
+   
+    alert("Usuário: " + email + "\nSenha: " + senha);
+}
